b26a9b1b04e31f4756ca8fad5402648d0a93a1ea
Alejandro Metke <alejandro.metke@csiro.au>
2020-12-17 16:27:26 +1000

    Externalised OAuth configuration parameters.

--- ui/src/keycloak.ts
+++ ui/src/keycloak.ts
@@ -1,11 +1,12 @@
+import env from '@beam-australia/react-env';
 import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: 'https://auth.ontoserver.csiro.au/auth',
-  realm: 'aehrc',
-  clientId: 'redmatch',
+  url: env('KEYCLOAK_URL'),
+  realm: env('KEYCLOAK_REALM'),
+  clientId: env('KEYCLOAK_CLIENT_ID'),
 });
 
 export default keycloak;
