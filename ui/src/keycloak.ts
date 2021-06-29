2a1a44b72c718409296667f4c3f4fe4cf2309e61
Alejandro Metke <alejandro.metke@csiro.au>
2021-06-30 09:25:24 +1000

    Removed dependency to react-env.

--- ui/src/keycloak.ts
+++ ui/src/keycloak.ts
@@ -3,15 +3,14 @@
  * Organisation (CSIRO) ABN 41 687 119 230. Licensed under the CSIRO Open Source
  * Software Licence Agreement.
  */
-import env from '@beam-australia/react-env';
 import Keycloak from 'keycloak-js';
 
 // Setup Keycloak instance as needed
 // Pass initialization options as required or leave blank to load from 'keycloak.json'
 const keycloak = Keycloak({
-  url: env('KEYCLOAK_URL'),
-  realm: env('KEYCLOAK_REALM'),
-  clientId: env('KEYCLOAK_CLIENT_ID'),
+  url: process.env.REACT_APP_KEYCLOAK_URL!,
+  realm: process.env.REACT_APP_KEYCLOAK_REALM!,
+  clientId: process.env.REACT_APP_KEYCLOAK_CLIENT_ID!,
 });
 
 export default keycloak;
